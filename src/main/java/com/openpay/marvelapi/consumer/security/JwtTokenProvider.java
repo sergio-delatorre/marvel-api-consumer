95471c3e415086bd7c76b490d68d6c23fd742637
Sergio de la Torre <sergio.delatorre@unmsm.edu.pe>
2024-05-25 19:47:57 -0500

    feat: Add security config & JWT token provider

---
+++ src/main/java/com/openpay/marvelapi/consumer/security/JwtTokenProvider.java
@@ -0,0 +1,36 @@
+package com.openpay.marvelapi.consumer.security;
+
+import io.jsonwebtoken.JwtException;
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.stereotype.Component;
+
+import java.util.Date;
+
+@Component
+public class JwtTokenProvider {
+
+    private static final String SECRET_KEY = "sA5kFx4cxyQWP9t2fVZW6pv87UVtbd2BnsQAgHlYZ8g=";
+    private static final long EXPIRE_TIME_MS = 3600000;
+
+    public String generateToken() {
+        Date now = new Date();
+        Date expiration = new Date(now.getTime() + EXPIRE_TIME_MS);
+
+        return Jwts.builder()
+                .setSubject("user")
+                .setIssuedAt(now)
+                .setExpiration(expiration)
+                .signWith(SignatureAlgorithm.HS256, SECRET_KEY)
+                .compact();
+    }
+
+    public boolean validateToken(String token) {
+        try {
+            Jwts.parser().setSigningKey(SECRET_KEY).parseClaimsJws(token);
+            return true;
+        } catch (JwtException | IllegalArgumentException e) {
+            return false;
+        }
+    }
+}
