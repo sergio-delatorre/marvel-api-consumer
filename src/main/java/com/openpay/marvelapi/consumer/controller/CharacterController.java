4fa89fdf68d134a2305803ca198e147e2573e3ae
Sergio de la Torre <sergio.delatorre@unmsm.edu.pe>
2024-05-25 19:47:15 -0500

    feat: Get Characters calling Marvel API Client Proof of Concept

---
+++ src/main/java/com/openpay/marvelapi/consumer/controller/CharacterController.java
@@ -0,0 +1,28 @@
+package com.openpay.marvelapi.consumer.controller;
+
+import com.openpay.marvelapi.client.model.dto.CharacterResponse;
+import com.openpay.marvelapi.consumer.service.MarvelService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/marvel/characters")
+public class CharacterController {
+
+    private final MarvelService marvelService;
+
+    @Autowired
+    public CharacterController(MarvelService marvelService) {
+        this.marvelService = marvelService;
+    }
+
+    @GetMapping()
+    public List<CharacterResponse> getCharacters() {
+        return marvelService.getCharacters();
+    }
+
+}
---
+++ src/main/java/com/openpay/marvelapi/consumer/service/MarvelServiceImpl.java
@@ -0,0 +1,24 @@
+package com.openpay.marvelapi.consumer.service;
+
+import com.openpay.marvelapi.client.model.dto.CharacterResponse;
+import com.openpay.marvelapi.client.service.CharacterService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class MarvelServiceImpl implements MarvelService {
+
+    private CharacterService characterService;
+
+    @Autowired
+    public MarvelServiceImpl(CharacterService characterService) {
+        this.characterService = characterService;
+    }
+
+    @Override
+    public List<CharacterResponse> getCharacters() {
+        return characterService.getCharacters();
+    }
+}
